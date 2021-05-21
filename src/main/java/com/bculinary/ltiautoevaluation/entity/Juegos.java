28ecaff3090115a02ec0e6cb02c0930faff5da4a
Iván Gómez <igomez@entornosdeformacion.com>
2021-05-21 09:35:51 +0200

    Arreglo clase Juegos

--- src/main/java/com/bculinary/ltiautoevaluation/entity/Juegos.java
+++ src/main/java/com/bculinary/ltiautoevaluation/entity/Juegos.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
@@ -18,7 +19,7 @@ import lombok.NoArgsConstructor;
 public class Juegos implements Serializable {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id_juego;
 	private String titulo;
 	private int anyo_publicacion;
